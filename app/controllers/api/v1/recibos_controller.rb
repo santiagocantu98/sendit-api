12ca80971f50d66b45250b2a4cab8580e4547cbd
santiagocantu98 <santiagocantu98@gmail.com>
2020-12-15 17:03:53 -0600

    adding viaje id in recibo params

--- app/controllers/api/v1/recibos_controller.rb
+++ app/controllers/api/v1/recibos_controller.rb
@@ -46,6 +46,6 @@ class Api::V1::RecibosController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def recibo_params
-      params.require(:recibo).permit(:subtotal, :tarifa, :iva, :total)
+      params.require(:recibo).permit(:subtotal, :tarifa, :iva, :total, :viaje_id)
     end
 end
