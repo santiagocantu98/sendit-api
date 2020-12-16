5138a932667c3dee4f9cbb920b5f7c230ac91898
santiagocantu98 <santiagocantu98@gmail.com>
2020-12-16 02:42:14 -0600

    image to vehiculos

--- db/migrate/20201216084141_add_image_to_vehiculos.rb
+++ db/migrate/20201216084141_add_image_to_vehiculos.rb
@@ -1,5 +1,5 @@
 class AddImageToVehiculos < ActiveRecord::Migration[6.0]
   def change
-    add_column :vehiculos, :imagenVehiculo, :text, :limit => 4000000
+    add_column :vehiculos, :imagenVehiculo, :text
   end
 end
